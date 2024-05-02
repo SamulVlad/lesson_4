b5aa63c74f80a5b899c88ad5245fd1b312715e92
SamulVlad <55761105+SamulVlad@users.noreply.github.com>
2024-05-02 09:01:54 +0300

    Add files via upload

--- src/com/samul/javaсorelessons/lesson4/SelfStudyTask7.java
+++ src/com/samul/javaсorelessons/lesson4/SelfStudyTask7.java
@@ -5,14 +5,14 @@ package com.samul.javaсorelessons.lesson4;
 public class SelfStudyTask7 {
 
 	public static void main(String[] args) {
-		int number = 12312412, tempNumber;
+		int number = 712312412, tempNumber;
 		int product = 1;
 
 		int dgt;
 
 		tempNumber = number;
 		while (tempNumber >= 1) {
-			dgt = (int) Math.round((tempNumber / 10.0 - tempNumber / 10) * 10);
+			dgt = tempNumber % 10;
 			tempNumber /= 10;
 			product *= dgt;
 		}
